4ed445ff5162679a60cdbb86cc13ebf441687152
snehapkamble <snehapkamble@gmail.com>
2019-11-20 21:33:30 +0530

    formatted code

--- web/modules/custom/invoice/src/Controller/invoiceForm.php
+++ web/modules/custom/invoice/src/Controller/invoiceForm.php
@@ -3,20 +3,23 @@
 namespace Drupal\invoice\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+
 /**
  * Description of ImporterFileUploadReport.
  */
 class invoiceForm extends ControllerBase {
-  function buildForm(){
-   $values = array('type' => 'invoice');
 
-	$node = \Drupal::entityTypeManager()
-	  ->getStorage('node')
-	  ->create($values);
+  function buildForm() {
+    //get the of node type
+    $type = array('type' => 'invoice');
+    $node = \Drupal::entityTypeManager()
+            ->getStorage('node')
+            ->create($type);
+    //get default form for Content type invoice
+    $form = \Drupal::entityTypeManager()
+            ->getFormObject('node', 'default')
+            ->setEntity($node);
+    return \Drupal::formBuilder()->getForm($form);
+  }
 
-	$form = \Drupal::entityTypeManager()
-	  ->getFormObject('node', 'default')
-	  ->setEntity($node);
-	return \Drupal::formBuilder()->getForm($form);
-	}
 }
